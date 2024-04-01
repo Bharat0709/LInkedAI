69324c48bcd3964b4263ad2a8f3a81d95a3c315d
Bharat Pahwa <pahwabharat15@gmail.com>
2024-04-02 02:50:23 +0530

    feat/collections

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { Collection, Post } = require("./../models/collection"); // Import the Mongoose models
 const User = require("./../models/userModel");
 const GuestUser = require("../models/guestUser");
 const jwt = require("jsonwebtoken");
@@ -269,6 +270,150 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
 
 exports.activateServer = catchAsync(async (req, res, next) => {
   res.status(200).json({ server: "Server is active" });
+});
+
+// Function to add a new post to a collection
+exports.addtocollection = catchAsync(async (req, res, next) => {
+  try {
+    const userId = req.user._id; // Assuming user ID is available in req.user
+    const { collectionId, collectionName, postURL, postDescription } = req.body;
+    console.log({ collectionId, collectionName, postURL, postDescription });
+
+    let collection;
+
+    // If the collectionId is provided, find the collection by ID
+    if (collectionId && collectionId != null) {
+      collection = await Collection.findById(collectionId);
+      console.log(collection);
+    } else {
+      // If the collectionId is not provided, create a new collection
+      collection = new Collection({
+        name: collectionName,
+        userId: userId,
+      });
+
+      // Check if the user has reached the limit of 5 collections
+      const userCollectionsCount = await Collection.countDocuments({
+        userId: userId,
+      });
+      if (userCollectionsCount >= 5) {
+        throw new Error("Maximum limit of 5 collections reached");
+      }
+
+      await collection.save();
+    }
+
+    // Check if the collection has reached the limit of 10 posts
+    if (collection.posts.length >= 10) {
+      throw new Error("Maximum limit of 10 posts per collection reached");
+    }
+
+    // Create a new post
+    const newPost = new Post({
+      url: postURL,
+      description: postDescription,
+    });
+
+    // Add the new post to the collection
+    collection.posts.push(newPost);
+    await collection.save();
+    res.status(200).json({ collection });
+  } catch (error) {
+    console.error("Error adding post:", error);
+    throw new Error("Failed to add post");
+  }
+});
+
+exports.browseCollections = catchAsync(async (req, res, next) => {
+  try {
+    const userId = req.user.id; // Assuming user ID is available in req.user
+
+    // Find all collections associated with the user
+    const collections = await Collection.find({ userId: userId });
+
+    res.status(200).json({ collections });
+  } catch (error) {
+    console.error("Error browsing collections:", error);
+    throw new Error("Failed to browse collections");
+  }
+});
+
+exports.deleteCollectionPost = catchAsync(async (req, res, next) => {
+  const userId = req.user._id; // Assuming the user ID is stored in req.user._id
+  const collectionId = req.params.collectionId;
+  const postId = req.params.postId;
+
+  try {
+    // Find the collection by its ID
+    const collection = await Collection.findById(collectionId);
+
+    if (!collection) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Collection not found" });
+    }
+
+    // Check if the collection belongs to the user
+    if (!collection.userId.equals(userId)) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Unauthorized access" });
+    }
+
+    // Find the post within the collection by its ID
+    const post = collection.posts.find((post) => post._id.equals(postId));
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found in the collection" });
+    }
+
+    // Remove the post from the collection
+    collection.posts.pull(post._id);
+    await collection.save();
+
+    // Delete the post document from the database
+    await Post.findByIdAndDelete(postId);
+
+    res
+      .status(200)
+      .json({ success: true, message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error deleting post" });
+  }
+});
+
+exports.deleteCollection = catchAsync(async (req, res, next) => {
+  const collectionId = req.params.collectionId;
+
+  try {
+    const collection = await Collection.findById(collectionId);
+
+    // Check if the collection exists
+    if (!collection) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Collection not found" });
+    }
+
+    // Check if the collection belongs to the current user
+    if (collection.userId.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ success: false, message: "Unauthorized access" });
+    }
+
+    // Delete the collection
+    await Collection.findByIdAndDelete(collectionId);
+    res
+      .status(200)
+      .json({ success: true, message: "Collection deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Error deleting collection" });
+  }
 });
 
 // RESTRICTING THE USER FROM DELETING THE TOURS BASEDON THEIR ROLES
