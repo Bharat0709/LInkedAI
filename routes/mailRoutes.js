dafaa6e273ba374db2aefddda26ea367faa3d50d
Bharat Pahwa <pahwabharat15@gmail.com>
2024-05-16 12:54:41 +0530

    Add Early Access

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mailController = require('../controllers/mailController');
 const Router = express.Router();
-Router.post('/submit/:mail', mailController.sendMailtoUser);
+Router.post('/submit/:mail', mailController.sendAccess);
 Router.post('/otp/:mail', mailController.sendOTPtoUser);
 module.exports = Router;
