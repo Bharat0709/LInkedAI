cd52e5b46ddc9ba90baf627e12c6a9bf34d7dcd8
Bharat Pahwa <letsbunktoday@gmail.com>
2025-11-12 15:59:54 +0530

    Remove email invitation on new member creation
    Removed email invitation sending after member creation.

--- services/Member/memberService.js
+++ services/Member/memberService.js
@@ -62,9 +62,6 @@ const createMember = async (organizationId, memberData) => {
     organizationId,
     createdAt: new Date(),
   });
-
-  sendNewMemberInviteEmail(organization.name, name, email, member.connectionToken);
-
   return member;
 };
 
