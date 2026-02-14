5d1ac9c8b14d60549a2ff897202c7da1c7518777
Bharat Pahwa <letsbunktoday@gmail.com>
2026-02-14 22:06:41 +0530

    Update authHelper.js

--- services/Auth/authHelper.js
+++ services/Auth/authHelper.js
@@ -116,7 +116,7 @@ const createGoogleOrganizationData = profile => {
 
     // Default plan features
     planFeatures: {
-      aiModels: ['gemini', 'chatgpt'],
+      aiModels: ['chatgpt'],
       hasPrioritySupport: false,
       canBuyCredits: true,
     },
