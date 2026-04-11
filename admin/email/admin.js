f53631a7de68cc4a73c9d5cdb88b088f249714ff
Bharat Pahwa <letsbunktoday@gmail.com>
2026-04-12 01:19:37 +0530

    Added new Updates for 3.1 Version

--- admin/email/admin.js
+++ admin/email/admin.js
@@ -95,5 +95,5 @@ exports.send3_0UpdateEmail = async user => {
     year: new Date().getFullYear(),
   });
 
-  return await sendAutoSendEmail(user.email, '🚀 EngageGPT 3.0 is Here: Smaller, Faster, and Stable!', html);
+  return await sendAutoSendEmail(user.email, '🚀 EngageGPT 3.1 is Here: GO AND HIT GENERATE COMMENT!', html);
 };
